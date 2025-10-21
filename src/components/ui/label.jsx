166f2dccde0fd1ec1ca1e21f072f8eb1a8b98c40
jodeal24 <dealmeida.joao@hotmail.com>
2025-10-21 05:19:11 +0200

    Delete src/components/ui/label.jsx

--- src/components/ui/label.jsx
+++
@@ -1,6 +0,0 @@
-// src/components/ui/label.jsx
-import React from "react";
-
-export function Label({ className = "", ...props }) {
-  return <label className={`text-sm font-medium ${className}`} {...props} />;
-}
